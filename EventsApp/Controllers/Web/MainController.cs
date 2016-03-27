211e4779b9a7dabbe56fa2d5c589d0a84507bd1f
Ricky Stam <k.stam@hotmail.com>
2016-03-28 00:01:37 +0300

    typo fix

--- EventsApp/Controllers/Web/MainController.cs
+++ EventsApp/Controllers/Web/MainController.cs
@@ -7,7 +7,7 @@ namespace EventsApp.Controllers
     public class MainController : Controller
     {
         public ActionResult Index() {
-            //I add a cookie HTTP POST ONLY just to demostrate in EventsController a Custom OnAuthorization
+            //I add a cookie HTTP POST ONLY just to demonstrate in EventsController a Custom OnAuthorization
             //I'll check if this cookie with the specific value exists and only then i'll grand access
             Response.Cookies.Add(new System.Web.HttpCookie(Config.AdeleCookieName, Config.AdeleCookieValue) {
                 Path = "/",
